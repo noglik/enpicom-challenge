4f2164bdf4bf6c3a37ea1fee148e603953c1ece8
noglik <yaroslaubuk@gmail.com>
2022-09-13 23:07:10 +0200

    Add testcontainers to functional test run

--- server/test/mocha-setup.ts
+++ server/test/mocha-setup.ts
@@ -1,8 +1,10 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import chaiHttp from 'chai-http';
-import { createServer } from '../src/app';
 import { AddressInfo, Server } from 'net';
+import { GenericContainer } from 'testcontainers';
+import { createServer } from '../src/app';
+import { setup as setupDb, close as closeDb } from '../src/db';
 
 chai.use(chaiAsPromised);
 chai.use(chaiHttp);
@@ -11,10 +13,27 @@ declare module 'mocha' {
     export interface Context {
         serverInstance: Server;
         server: ChaiHttp.Agent;
+        container: Awaited<ReturnType<GenericContainer['start']>>;
     }
 }
 
-before(function () {
+const DB_NAME = 'enpicom';
+const DB_PASSWORD = 'password';
+const PORT = 5432;
+
+before(async function () {
+    this.timeout(20000);
+    this.container = await new GenericContainer('postgres:14.5-alpine')
+        .withExposedPorts(PORT)
+        .withEnv('POSTGRES_DB', DB_NAME)
+        .withEnv('POSTGRES_PASSWORD', DB_PASSWORD)
+        .start();
+
+    const host = this.container.getHost();
+    const port = this.container.getMappedPort(PORT);
+
+    setupDb(`postgres://postgres:${DB_PASSWORD}@${host}:${port}/${DB_NAME}`);
+
     this.serverInstance = createServer().listen();
     const address = this.serverInstance.address as unknown as AddressInfo;
     const localUrl = `http://localhost:${address.port}`;
@@ -23,4 +42,6 @@ before(function () {
 
 after(function () {
     this.serverInstance.close();
+    closeDb();
+    this.container.stop();
 });
