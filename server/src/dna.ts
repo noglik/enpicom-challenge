1d125d59c3dc6d526cb11e68b168710e8e9487c5
noglik <yaroslaubuk@gmail.com>
2022-09-15 13:03:20 +0200

    Handle querying for dna sequence

--- server/src/dna.ts
+++ server/src/dna.ts
@@ -4,7 +4,8 @@ import { db } from './db';
 import { logger } from './logger';
 
 const INTERNAL_SERVER_ERROR = 500;
-const ADD_ERROR_MESSAGE = 'Cannot add dna sequence';
+const INSERT_ERROR_MESSAGE = 'Cannot insert dna sequence';
+const QUERY_ERROR_MESSAGE = 'Cannot query dna sequence';
 const GENERIC_ERROR_RESPONSE = 'Something went wrong';
 
 export const dnaRouter = Router();
@@ -18,7 +19,23 @@ dnaRouter.post('/', async (req, res) => {
         );
         return res.json({ id: queryResult.rows[0].id });
     } catch (err) {
-        logger.error({ err }, ADD_ERROR_MESSAGE);
+        logger.error({ err }, INSERT_ERROR_MESSAGE);
+        return res.status(INTERNAL_SERVER_ERROR).send({ message: GENERIC_ERROR_RESPONSE });
+    }
+});
+
+// query dna
+dnaRouter.get('/', async (req, res) => {
+    const { sequence } = req.query;
+    try {
+        const queryResult = await db.query(
+            sql`SELECT id, sequence FROM dna
+                WHERE sequence LIKE ${`%${sequence}%`}
+                ORDER BY sequence`
+        );
+        return res.json(queryResult.rows);
+    } catch (err) {
+        logger.error({ err, sequence }, QUERY_ERROR_MESSAGE);
         return res.status(INTERNAL_SERVER_ERROR).send({ message: GENERIC_ERROR_RESPONSE });
     }
 });
--- server/test/dna.test.ts
+++ server/test/dna.test.ts
@@ -1,20 +1,44 @@
 import { expect } from 'chai';
+import sql from 'sql-template-strings';
+import { db } from '../src/db';
 
 describe('Dna router', function () {
-    it('POST /api/dna should create dna record in db on', async function () {
-        const sequence = 'ACTG';
-        const res = await this.server.post('/api/dna').send({ sequence });
+    describe('POST /api/dna save', function () {
+        it('should create dna record in db on', async function () {
+            const sequence = 'ACTG';
+            const res = await this.server.post('/api/dna').send({ sequence });
 
-        expect(res.status).to.be.eql(200);
-        expect(res.body).to.have.property('id');
-        expect(res.body.id).to.be.a('number');
+            expect(res.status).to.be.eql(200);
+            expect(res.body).to.have.property('id');
+            expect(res.body.id).to.be.a('number');
+        });
+
+        it('should return 500 if sequence is too long', async function () {
+            const sequence = 'ACTG'.repeat(64); // 256 length string
+            const res = await this.server.post('/api/dna').send({ sequence });
+
+            expect(res.status).to.be.eql(500);
+            expect(res.body).to.be.eql({ message: 'Something went wrong' });
+        });
     });
 
-    it('POST /api/dna should return 500 if sequence is too long', async function () {
-        const sequence = 'ACTG'.repeat(64); // 256 length string
-        const res = await this.server.post('/api/dna').send({ sequence });
+    describe('GET /api/dna query', function () {
+        it('should query already existing dna sequences', async function () {
+            const { rows } = await db.query(
+                sql`INSERT INTO dna(sequence) VALUES('TCAGTCAG'), ('ACTGTCAG') RETURNING id, sequence`
+            );
 
-        expect(res.status).to.be.eql(500);
-        expect(res.body).to.be.eql({ message: 'Something went wrong' });
+            const res = await this.server.get('/api/dna').query({ sequence: 'TCAG' });
+
+            expect(res.status).to.be.eql(200);
+            expect(res.body).to.be.eql(rows.reverse()); // check ordering too
+        });
+
+        it("should return empty array if pattern doesn't match", async function () {
+            const res = await this.server.get('/api/dna').query({ sequence: 'CAGTACGTTCAG' });
+
+            expect(res.status).to.be.eql(200);
+            expect(res.body).to.be.eql([]);
+        });
     });
 });
